b8b9d8d01515569e09cbdaedc027f57a2046c1b1
suresh-git07 <biswalsuresh2018@gmail.com>
2023-07-08 23:30:44 +0530

    first

---
+++ src/test/java/tiger/pinkey.java
@@ -0,0 +1,51 @@
+package tiger;
+
+  abstract    class   pinkey {
+
+	
+	 static  int i=20;
+	
+	public  static  void m1()
+	  {
+		  int j=44;
+		  System.out.println("is my sister");
+		 
+	  }
+	  
+	    public void m2()
+	  {
+		
+		  System.out.println("cute");
+		 
+	  }
+	  
+	  pinkey()
+	  {
+		  System.out.println("jerryyyyyyyyyyyy");
+	  }
+	  
+}
+ 
+ 
+ class papu extends pinkey{
+	 
+	 
+	
+	
+//		public  static void m1()
+//		{
+//		 
+//			System.out.println(" ddddddd");
+//		}
+		public   void m4()
+		{
+		 
+			
+		}
+//         
+	public   static void main(String[] args) {
+		
+		pinkey.m1();
+	}
+	 
+ }
